c4b4c5435e9d88dac5e3bf658bc3fc7493aaa7c2
PomboObeso <42008618+PomboObeso@users.noreply.github.com>
2019-11-24 11:24:35 -0300

    Comment Initial

--- Lista2/18.R
+++ Lista2/18.R
@@ -1,6 +1,6 @@
-pexp(5, rate = 1/5, lower.tail = FALSE) #A  
+pexp(5, rate = 1/5, lower.tail = FALSE) #A  A Distruição Exponencial para P(x > 5)
 
-pexp(4, rate = 1/5)   #B
+pexp(4, rate = 1/5)   #B A Distribuição Exponencial para P(x < 5)
 
 
-pexp(8, rate = 1/5) - pexp(3, rate = 1/5) #C
+pexp(8, rate = 1/5) - pexp(3, rate = 1/5) #C A Distribuição Exponencial para P(x >= 3 && x <= 8)
